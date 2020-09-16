d587e69229f8b0afa66cbbd9da57b47e9aed7f78
catalina <cdianacatalina@gmail.com>
2020-09-16 11:45:34 +0200

    add url to README.md

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+// The date and time is display to check connectivity with the html file.
 var dateToday = new Date();
 var date = dateToday.getDate() + "/" + (dateToday.getMonth()+1) + "/" + dateToday.getFullYear();
 var time = dateToday.getHours() + ":" + dateToday.getMinutes(); 
-
+// TODO -- add cero to minutes less than 10.
 document.getElementById("date-display").innerHTML ="The date today is " + date + " and the time " + time ;

5fb34be33ae42e333b01797e55e1f3ea7e598b2c
catalina <cdianacatalina@gmail.com>
2020-09-16 17:07:49 +0200

    corrections of 1st pull request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // The date and time is display to check connectivity with the html file.
 var dateToday = new Date();
-var date = dateToday.getDate() + "/" + (dateToday.getMonth()+1) + "/" + dateToday.getFullYear();
-var time = dateToday.getHours() + ":" + dateToday.getMinutes(); 
-// TODO -- add cero to minutes less than 10.
-document.getElementById("date-display").innerHTML ="The date today is " + date + " and the time " + time ;
+var date = `${dateToday.getDate()}/${(dateToday.getMonth()+1)}/${dateToday.getFullYear()}`;
+
+// en-US: uses 12-hour time with AM/PM
+var time = dateToday.toLocaleTimeString('en-US', {hour:'2-digit', minute:'2-digit'});
+document.getElementById("date-display").innerHTML = `The date today is ${date} and the time ${time}`;

213207da56345d04aefbe1bfdd2d4e98d00422e6
catalina <cdianacatalina@gmail.com>
2020-09-23 16:45:16 +0200

    got temperature by calling the Open Weather API

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ document.getElementById("date-display").innerHTML = `The date today is ${date} a
 
 // HTML Geolocation API on your browser - no web request!
 // gettting location of the user's computer: latitude and longitude
-function printLocation(pos) {
-    var lat = pos.coords.latitude.toFixed(2);
-    var long = pos.coords.longitude.toFixed(2);
+function getLocation(pos) {
+    let lat = pos.coords.latitude.toFixed(2);
+    let long = pos.coords.longitude.toFixed(2);
     document.getElementById("location").innerHTML = 
-            `Your position is lat: ${lat}° and long: ${long}°`;
+            `Your location is lat: ${lat}° and long: ${long}°`;
+    getTemp(lat,long);
 }
 
 function handleError(error) {
@@ -22,5 +23,36 @@ function handleError(error) {
     console.warn(`Error displaying position, error: ${error.code}`)
 }
 
-navigator.geolocation.getCurrentPosition(printLocation, handleError);
+navigator.geolocation.getCurrentPosition(getLocation, handleError);
+
+// Getting the temperature from the Open Weather API using geographical coordinates.
+// Response format is Json by default.
+// https://openweathermap.org/current --free
+
+const KEY = "3137461397c70af0e13c77fda97afa11"
+
+function getTemp(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}&units=metric`
+    //the fetch() method instructs the web browsers to send a request to a URL.
+    //less than60 calls/min
+    fetch(api)
+    .then(response => {
+        // handles response
+        // console.log(response.statusText); when response.status is 200, it is OK.
+        if (response.status != 200){
+            console.log("there is a problem with the request")
+        }else{
+            // data --> Promise object represents the eventual completion (or failure) 
+            // of an asynchronous operation and its resulting value.
+            let data = response.json().then(data=> {
+            console.log(`Request succeed - json: ${data}`)    
+            document.getElementById("temperature").innerHTML = `The 
+            temperature in your location is ${data.main.temp.toFixed(2)}C°`}) 
+        }        
+    })
+    .catch(error => {
+        // handles errors
+        console.warn(`error: ${error.message}, weather API`)  
+    });
+}
 
